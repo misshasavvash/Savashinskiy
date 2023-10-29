59371b69126b0fca21adf7a4fa5aaf489e022e67
misshasavvash <146664036+misshasavvash@users.noreply.github.com>
2023-10-30 00:08:41 +0500

    Add files via upload

---
+++ лаб. работа 5.py
@@ -0,0 +1,29 @@
+def caesar_cipher(message, key):
+    encrypted_message = ""
+    i = 0
+    while i < len(message):
+        char = message[i]
+        if char.isalpha():
+            if char.islower():
+                encrypted_char = chr((ord(char) - 97 + key) % 26 + 97)
+            else:
+                encrypted_char = chr((ord(char) - 65 + key) % 26 + 65)
+            encrypted_message += encrypted_char
+        else:
+            encrypted_message += char
+        i += 1  
+    return encrypted_message
+def caesar_decipher(encrypted_message, key): # Функция для расшифровки текста
+    decrypted_message = ""
+def main(): # Функция main() является точкой входа в программу
+    message = input("Введите текст: ")
+    key = int(input("Введите шаг сдвига от 1 до 33: "))
+    if key < 1 or key > 33: # Проверяем, что шаг сдвига находится в допустимых пределах (от 1 до 33)
+        print("Ошибка: недопустимый шаг сдвига!")
+        return None
+    encrypted_message = caesar_cipher(message, key)  
+    decrypted_message = caesar_decipher(encrypted_message, key)
+    print("Зашифрованный текст:", encrypted_message)
+    print("Расшифрованный текст:", decrypted_message) 
+if __name__ == "__main__":
+    main()
